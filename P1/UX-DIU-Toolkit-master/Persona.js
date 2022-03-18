a09a6f37b8406bbd8c81b13cf148fea3ac2fabd5
rafaguzmanval <rafagval@gmail.com>
2022-03-18 16:58:05 +0100

    mejor

---
+++ P1/UX-DIU-Toolkit-master/Persona.js
@@ -0,0 +1,112 @@
+/*******************************************/
+/*             PERSONA.JS                  */
+/*     Datos para PERSONA TEMPLATE         */   
+/*          [DIU] UX Toolkit v1.0 2019     */                        
+/*          ver 1.2 26/Feb/2022            */
+/*******************************************/
+    
+/****  README:       */
+/****  Modifica los datos para las Personas      */
+/****  v.1.1 Incluye nombre de tu grupo de prácticas (Grupo.ID), curso académico y enlace a github ***/
+/****  Las imagenes para  'Photo'  están en carpeta ./photos **/
+/****  Si se usan nuevas imágenes se deben añadir a esa carpeta **/
+/****  Los valores de rating están entre 1..5 **/
+/****  recursos de imágenes:  https://www.vectorstock.com/royalty-free-vectors/vectors-by_zdeneksasek ***/
+
+
+
+angular.module("angular", [])
+	.controller("controller", ["$scope", function($scope) { 
+        $scope.Grupo_ID ="DIU1.ABCDEF";
+        $scope.Curso ="2021/22";
+        $scope.Github_ID ="https://github.com/mgea/UX-DIU-Toolkit";
+        
+		$scope.PersonaIndex = 0;
+		$scope.Personas = [
+			{		
+                
+                
+                /*************************************/
+                /**** PRIMERA PERSONA          *******/
+                /*** Cambiar datos             *******/
+                /*************************************/
+                
+                
+				Id: 0,
+				Name: "Isaac",
+				Photo: "man.png",
+				Quote: "La vida son dos días",
+				Age: 26,
+				Occupation: "Coach de surf",
+				Family: "Con pareja desde hace 3 meses",
+				Location: "Cádiz (San Fernando)",
+				Character: "Apasionado de la música, extrovertido, creativo, amante de los deportes acuáticos",
+				PersonalityTraits: [
+					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 9 },
+					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 7 },
+					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 8 },
+					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 6 }
+				], 
+				Goals: ["Ir a Hawai a ganar el campeonato mundial de surf", "Prácticar también otro tipo de deportes tanto con la tabla de snowboard o como tenis"],
+				Frustrations: ["Le gustaría tocar la batería pero es bastante arrítmico", "Se lesionó y no pudo competir durante años, lo que bajó mucho su motivación"],
+				Bio: "Nació en Cádiz y desde los 6 años ya surfeaba las olas cuando le enseñó su tio. Estudió la FP del TFAD para convertirse en un buen instructor de surf",
+				Tech: [
+					{ Name: "TIC/Internet", Value: 2 },
+					{ Name: "Movil", Value: 2 },
+					{ Name: "RRSS", Value: 3 },
+					{ Name: "Software", Value: 2 }
+					
+				], 
+                Contextos: "Quiere visitar otros lugares de Andalucía y Granada era una buena opción para probar el tapeo y subir a la Sierra a prácticar Snowboard",  
+				PreferredChannels: [
+					{ Name: "Publicidad Tradicional", Value: 5 },
+					{ Name: "Online & Social Media", Value: 3 },
+					{ Name: "Recomendaciones & sugerencias", Value: 2 },
+					{ Name: "Persona confianza (amigos, boca a boca)", Value: 1 }
+				]
+			},
+			{	
+                
+                /*************************************/
+                /**** SEGUNDA PERSONA          *******/
+                /*** Cambiar datos             *******/
+                /*************************************/
+                
+                
+				Id: 1,
+				Name: "Monica Suarez",
+				Photo: "woman.png",
+				Quote: "A quotation that captures the essence of this person's personality",
+				Age: 17,
+				Occupation: "Searching for a cure for the Empress",
+				Family: "No parents, only family are the people who raised him.",
+				Location: "The Grassy Plains of Fantasia",
+				Character: "Strong, reliable and fearless.",
+				PersonalityTraits: [
+					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 3 },
+					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 3 },
+					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 2 },
+					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 2 }
+				], 
+				Goals: ["The goals this user hopes to achieve.", "A task that needs to be completed.", "A life goal to be reached.", "An experience to be felt."],
+				Frustrations: ["The frustrations this user would like to avoid.", "The obstacle that prevents the user from achieving their goals.", "The problems with the solutions already available.", "The product or service which does not currently exist."],
+				Bio: "The bio should be a short paragraph to describe the user journey. It should include some of their history leading up to a current use case. It may be helpful to incorporate information listed across the template and add pertinent details that may have been left out. Highlight factors of the user's personal and professional life that make this user an ideal customer of your product.",
+				Tech: [
+					{ Name: "TIC/Internet", Value: 5 },
+					{ Name: "Mobile", Value: 3 },
+					{ Name: "RRSS", Value: 3 },
+					{ Name: "Software", Value: 5 }
+					
+				], 
+                Contextos:   "The goals this user hopes to achieve." ,
+				PreferredChannels: [
+					{ Name: "Publicidad Tradicional (Ads)", Value: 5 },
+					{ Name: "Online & Social Media", Value: 2 },
+					{ Name: "Recomendaciones & sugerencias", Value: 2 },
+					{ Name: "Persona confianza (amigos, boca a boca)", Value: 2 }
+				]
+			}
+		];
+		$scope.model = $scope.Personas[0];
+
+	}])
